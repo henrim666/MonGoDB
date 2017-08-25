bb61dccc51676aff08f75b08328cd72bbf436034
92044h_m <92044h_m@WPJPL0613278.jp.intranet>
2017-08-25 15:28:09 +0900

    started hello world

---
+++ src/test/hello_world.java
@@ -0,0 +1,10 @@
+package test;
+
+public class hello_world {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+	
+}
